90299b6b770561453ed692cacb3ed3d4dbfff1ce
benzkittisak <benzkittisak@stv.ac.th>
2022-03-10 21:02:13 +0700

    update code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,18 @@
 import React, { useRef, useState, useEffect } from "react";
 import Isotope from "isotope-layout";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useLocation } from "react-router-dom";
 
-import {
-  setActiveClassOnTab,
-} from "../../utils";
+import { setActiveClassOnTab } from "../../utils";
 
 import GAME_LIST from "../../data/game.data";
 import GameCard from "../../components/game-card/game-card.component";
 
 const ShopPage = () => {
-  const [location , setLocation] = useSearchParams();
-  const [data , setData ] = useState(GAME_LIST);
-  
+  const [location, setLocation] = useSearchParams();
+  const [data, setData] = useState(GAME_LIST);
   const isotope = useRef();
   const [filterKey, setFilterKey] = useState("*");
+
 
   useEffect(() => {
     isotope.current = new Isotope(".filter-container", {
@@ -28,12 +26,14 @@ const ShopPage = () => {
     filterKey === "*"
       ? isotope.current.arrange({ filter: "*" })
       : isotope.current.arrange({ filter: `.${filterKey}` });
-
-    if(location.get('search')){
-      setData(GAME_LIST.filter(game => game.name.toLowerCase().includes(location.get('search').toLowerCase())));
+    if (location.get("search")) {
+      setData(
+        GAME_LIST.filter((game) =>
+          game.name.toLowerCase().includes(location.get("search").toLowerCase())
+        )
+      );
     }
-  }, [filterKey , location]);
-
+  }, [filterKey, location]);
 
   const handleFilterKeyChange = (key) => {
     setFilterKey(key);
@@ -84,7 +84,7 @@ const ShopPage = () => {
 
         <div className="row filter-container">
           {data.map((data, index) => (
-            <GameCard key={index} data={data}/>
+            <GameCard key={index} data={data} />
           ))}
         </div>
       </div>
