a73342da6ab7000a45a89e91dd6fc25d32f8b191
benzkittisak <benzkittisak@stv.ac.th>
2022-03-10 20:35:23 +0700

    Deploy3

--- src/components/home-tab/home-tab.component.jsx
+++ src/components/home-tab/home-tab.component.jsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
 import Tab from "react-bootstrap/Tab";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 import BestSell from "../bestSell/best-sell.component";
 import NewGame from "../new-game/new-game.component";
 
-import { getBestSell, getNewGame , getGameInfo } from "../../utils";
+import { getBestSell, getNewGame, getGameInfo } from "../../utils";
 import TabInfo from "../tab-info/tab-info.component";
 
 const HomeTab = () => {
   const [showData, setShowData] = useState(null);
-  const [gameId , setGameId] = useState(null);
+  const [gameId, setGameId] = useState(null);
 
   useEffect(() => {
     setShowData(getGameInfo(gameId));
-  }, [gameId ]);
+  }, [gameId]);
 
   return (
     <div className="py-4">
@@ -24,12 +25,18 @@ const HomeTab = () => {
             <div className="col-lg-12">
               <Nav variant="pill" className="home-tab-section text-center">
                 <Nav.Item>
-                  <Nav.Link eventKey="newGame" onClick={() => setShowData(null)}>
+                  <Nav.Link
+                    eventKey="newGame"
+                    onClick={() => setShowData(null)}
+                  >
                     <h6>ใหม่ยอดนิยม</h6>
                   </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="bestSell" onClick={() => setShowData(null)}>
+                  <Nav.Link
+                    eventKey="bestSell"
+                    onClick={() => setShowData(null)}
+                  >
                     <h6>ขายดี</h6>
                   </Nav.Link>
                 </Nav.Item>
@@ -45,23 +52,29 @@ const HomeTab = () => {
                   <Tab.Pane eventKey="bestSell">
                     <BestSell data={getBestSell()} loadGame={setGameId} />
                     <div className="tab-more">
-                        <button type="button" className="btn-tab-more">
-                          ดูเพิ่มเติม  
-                        </button>  
+                      <Link
+                        to={process.env.PUBLIC_URL + "/shop"}
+                        className="btn-tab-more"
+                      >
+                        ดูเพิ่มเติม
+                      </Link>
                     </div>
                   </Tab.Pane>
                   <Tab.Pane eventKey="newGame">
-                    <NewGame data={getNewGame()} loadGame={setGameId}/>
+                    <NewGame data={getNewGame()} loadGame={setGameId} />
                     <div className="tab-more">
-                        <button type="button" className="btn-tab-more">
-                          ดูเพิ่มเติม  
-                        </button>  
+                      <Link
+                        to={process.env.PUBLIC_URL + "/shop"}
+                        className="btn-tab-more"
+                      >
+                        ดูเพิ่มเติม
+                      </Link>
                     </div>
                   </Tab.Pane>
                 </Tab.Content>
               </div>
               <div className="col-lg-4">
-                <TabInfo  data={showData}/>
+                <TabInfo data={showData} />
               </div>
             </div>
           </div>
