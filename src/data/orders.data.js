dd52b73d25b31d37e5d8526caf6e794f6b8ab31c
NuchanonTangjai <6206021611109.fitm.kmutnb.ac.th>
2022-05-02 21:04:46 +0700

    อัปเดท server รูป

--- src/data/orders.data.js
+++ src/data/orders.data.js
@@ -3,7 +3,7 @@ const ORDERS_DATA = [
         id:1,
         order_date:'9/03/2565',
         price:1078,
-        poster:'https://i3.fpic.cc/file/img-b1/2022/03/08/egs-sifustandardedition-sloclap-g1c-00-1920x1080-03d1e653ea90.png',
+        poster:'https://cdn.discordapp.com/attachments/901805974459928608/950647775589199923/egs-sifustandardedition-sloclap-g2-00-1920x1080-edb885b62a36.png',
         product_name:'SIFU',
         quantity:2,
         status:'ชำระเงินแล้ว'
@@ -12,7 +12,7 @@ const ORDERS_DATA = [
         id:2,
         order_date:'14/03/2565',
         price:1290,
-        poster:'https://i3.fpic.cc/file/img-b1/2022/03/08/EGS_GodofWar_SantaMonicaStudio_S2_1200x1600-fbdf3cbc2980749091d52751ffabb7b7_1200x1600-fbdf3cbc2980749091d52751ffabb7b7.png',
+        poster:'https://cdn.discordapp.com/attachments/901805974459928608/950643439891005480/EGS_GodofWar_SantaMonicaStudio_S2_1200x1600-fbdf3cbc2980749091d52751ffabb7b7_1200x1600-fbdf3cbc2980749091d52751ffabb7b7.png',
         product_name:'God Of War',
         quantity:1,
         status:'ชำระเงินแล้ว'
@@ -21,7 +21,7 @@ const ORDERS_DATA = [
         id:3,
         order_date:'20/03/2565',
         price:1490,
-        poster:'https://cdn.discordapp.com/attachments/950973417216180244/951108913137324192/elden-ring-pc-game-steam-europe-cover.png',
+        poster:'https://cdn.discordapp.com/attachments/901805974459928608/950644986561572954/phvVT0qZfcRms5qDAk0SI3CM.png',
         product_name:'Elden Ring',
         quantity:2,
         status:'ชำระเงินแล้ว'
